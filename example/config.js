9152e5a7da3d21999b4cbbffaba10cc4034b3755
Chris Dawson <xrdawson@gmail.com>
2011-01-05 11:24:12 -0800

    Removed js config file in favor of json

--- example/config.js
+++
@@ -1,6 +0,0 @@
-{ 'apiKey' : 'asdasdadadadadadasdasdasd',
-  'ignorePaths' : [ '/stylesheets', '/images', '/javascript', '/css', "/login" ],
-  'reentryPoint' : '/rpx_login',
-  'logoutPoint' :  '/logout',
-  'loginPage' : '/login/index.html',
-  'onSuccessfulLogin' : 'handleLogin' }
